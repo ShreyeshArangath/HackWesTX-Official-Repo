90014dcec3f18100d6c27ec7af3cce098b0bbafc
Shreyesh <imshreyesh17@gmail.com>
2019-12-19 10:25:02 -0600

    sponsorship section updated

--- src/components/Sponsorship.js
+++ src/components/Sponsorship.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Advertising from '../assets/advertising.svg'
+import Advertising from '../assets/megaphone.png'
 import Recruit from '../assets/human-resources.svg'
 import Test from '../assets/flask.svg'
 
@@ -8,22 +8,23 @@ export class Sponsorship extends Component {
         return (
             <div>
                 <h2 className="subtitle">Sponsor Us!</h2>
-                <div className="floats-container trigger">
+
+                <div className="sponsor-container trigger">
                     <img src={Advertising} alt='advert'></img>
                     <p>Increase brand awareness among students</p>
                 </div>     
 
-                <div className="floats-container-reverse">
+                <div className="sponsor-container-reverse">
                     <img className="flipped" src={Recruit} />
-                    <p className="flipped-p">Nullam aliquam, orci nec eleifend pellentesque, libero felis sodales velit, dictum vestibulum leo tortor in ex. Curabitur risus massa, fringilla at sapien non, volutpat malesuada elit. Cras a ligula vitae purus suscipit rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ipsum ante, suscipit at erat ac, mattis faucibus nisl.</p>
+                    <p className="flipped-p">Recruit talented developers</p>
             
                  </div>
 
 
-        <div className="floats-container">
+        <div className="sponsor-container">
            
                 <img src={Test} />
-                <p>Nullam aliquam, orci nec eleifend pellentesque, libero felis sodales velit, dictum vestibulum leo tortor in ex. Curabitur risus massa, fringilla at sapien non, volutpat malesuada elit. Cras a ligula vitae purus suscipit rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ipsum ante, suscipit at erat ac, mattis faucibus nisl.</p>
+                <p>Test your APIs and give demos</p>
                
         </div>       
 
