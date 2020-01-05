497a2c356ac55f3a2bad97b4d3c9f0936e79d5b4
Shreyesh <imshreyesh17@gmail.com>
2020-01-05 03:32:27 -0600

    application initialized

---
+++ src/pages/Application.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Application extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Application
