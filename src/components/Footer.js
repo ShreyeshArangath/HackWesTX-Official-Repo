c349937ce6f67d521ab769ae63ae379b1316c24e
Guysnacho <guysnacho2@gmail.com>
2021-08-05 20:17:15 -0500

    Updated social

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="footer-main" style={Style}>
            <div className="contact-us">
-               <div className="social-media">
+               {/* <div className="social-media">
                     <a href="https://www.facebook.com/hackwestx/" ><i className="fab fa-facebook-f fa-2x"></i></a>
                </div>
                <div class="social-media">
@@ -13,15 +13,17 @@ export default function Footer() {
                 <div class="social-media">
                     <a href="https://join.slack.com/t/hackwestx2020/shared_invite/enQtOTA3MzU1Nzc0MDE3LWYyZTM2ZjMzNjhkMTBlNjdhYmQzMzA5Yzk5NGQ1YjMyYmIyODUwNjMyYmFmNDY5ZTgwM2NlYWZjYTlmYzQ5NGY" ><i className="fab fa-slack fa-2x"></i></a>
                 </div>
-                <div class="social-media">
-                    <a href="hackwestx2019@gmail.com" ><i className="far fa-paper-plane fa-2x"></i></a>
-                </div>
+                
                 <div class="social-media">
                     <a href="https://www.instagram.com/hackwestx" ><i className="fab fa-instagram fa-2x"></i></a>
+                </div> */}
+                
+                <div class="social-media">
+                    <a href="admin@hackwestx.com" ><i className="far fa-paper-plane fa-2x"></i></a>
                 </div>
            </div>
            <div class="made-with-love">
-           <h6> <i class="far fa-copyright"></i> HackWesTX 2020</h6>
+           <h6> <i class="far fa-copyright"></i> HackWesTX 2022</h6>
            <h6>Made with&nbsp;<i class="fas fa-heart footer-love"></i>&nbsp;and <i
         class="fas fa-code footer-code"></i></h6>
            </div>
