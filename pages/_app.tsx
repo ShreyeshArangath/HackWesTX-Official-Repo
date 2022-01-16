f29373c164806efee487507075eff9b719f336de
Samuel Adetunji <guysnacho2@gmail.com>
2022-01-16 01:26:27 -0600

    Fix: Fix rendering error. Missing return

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "../lib/theme";
 
 function MyApp({ Component, pageProps }) {
-  <ChakraProvider theme={customTheme}>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  return(
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
