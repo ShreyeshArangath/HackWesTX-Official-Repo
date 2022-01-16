2aa8e6f9bcf8eb4f364a30cde30a54f635758918
Samuel Adetunji <guysnacho2@gmail.com>
2022-01-16 01:11:42 -0600

    Feat: Config ChakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import customTheme from "../lib/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <ChakraProvider theme={customTheme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
