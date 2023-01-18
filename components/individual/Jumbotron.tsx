a0afff6b4fdd6042e76e03a5e4df9669b9b9e3f0
Samuel Adetunji <guysnacho2@gmail.com>
2023-01-18 01:15:37 -0600

    Update jumbotron responsivity + assets

--- components/individual/Jumbotron.tsx
+++ components/individual/Jumbotron.tsx
@@ -8,63 +8,72 @@ const Jumbotron = () => {
 
   return (
     <Grid
-      item
-      height="90vh"
-      xs={12}
+      container
       sx={{
         backgroundColor: "black",
-        justifyContent: "space-around",
-        alignItems: "center",
-        display: "flex",
-        px: 4,
+        p: 3,
       }}
     >
       <Grid
         item
-        xs={6}
+        xs={12}
+        md={6}
         sx={{
           justifyContent: "space-around",
           display: "flex",
+          alignItems: "center",
+          py: 5,
         }}
       >
         <Image src={Logo} alt="Cactus" />
       </Grid>
-      <Grid item xs={6}>
-        <Typography
-          textAlign="center"
-          className={styles.glitch}
-          fontSize="4rem"
-        >
-          HACKWES<span className={styles.tx}>TX</span>
-        </Typography>
-        <Typography variant="h5" textAlign="center" className={styles.glitch}>
-          February 25th - February 26th
-        </Typography>
-        <Typography variant="h5" textAlign="center" className={styles.glitch}>
-          <span className={styles.tx}>Innovation Hub, Lubbock</span>
-        </Typography>
-        <Box
-          sx={{
-            justifyContent: "space-around",
-            display: "flex",
-          }}
-        >
-          <Button
-            variant="contained"
-            color="success"
-            size="large"
-            sx={{
-              color: theme.palette.success.contrastText,
-              my: 4,
-              mx: "auto",
-              width: "30%",
-            }}
-            href="https://my.forms.app/hackwestxforms/hackwestx-registration-spring23"
-            target="_blank"
-            referrerPolicy="no-referrer"
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          justifyContent: "space-around",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Typography
+            textAlign="center"
+            className={styles.glitch}
+            fontSize="3rem"
           >
-            Register Now!
-          </Button>
+            HACKWES<span className={styles.tx}>TX</span>
+          </Typography>
+          <Typography variant="h5" textAlign="center" className={styles.glitch}>
+            February 25th - February 26th
+          </Typography>
+          <Typography variant="h5" textAlign="center" className={styles.glitch}>
+            <span className={styles.tx}>Innovation Hub, Lubbock</span>
+          </Typography>
+          <Box
+            sx={{
+              justifyContent: "space-around",
+              display: "flex",
+            }}
+          >
+            <Button
+              variant="contained"
+              color="success"
+              size="large"
+              sx={{
+                color: theme.palette.success.contrastText,
+                my: 4,
+                mx: "auto",
+                width: "50%",
+              }}
+              href="https://my.forms.app/hackwestxforms/hackwestx-registration-spring23"
+              target="_blank"
+              referrerPolicy="no-referrer"
+            >
+              Register Now!
+            </Button>
+          </Box>
         </Box>
       </Grid>
     </Grid>
