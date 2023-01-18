14bf0100e4d10c83719c841a4285b1352cf3c746
Samuel Adetunji <guysnacho2@gmail.com>
2023-01-18 01:51:37 -0600

    Add about-us

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 import Jumbotron from "../components/individual/Jumbotron";
 
@@ -6,6 +7,29 @@ const HomePage = () => {
   return (
     <Grid container>
       <Jumbotron />
+      <Grid item xs={12} my={4}>
+        <Box width={{ xs: "75vw", md: "50vw" }} mx="auto">
+          <Typography variant="h3" textAlign="center">
+            Who are we?
+          </Typography>
+          <Typography variant="body1" textAlign="center">
+            HackWesTX III is Texas Tech University&apos;s second student-run
+            hackathon! This year, the hackathon will start Saturday, February
+            25th and end Sunday, February 26th. The hackathon will last 24 hours
+            total!
+          </Typography>
+          <Typography variant="h3" textAlign="center" mt={3}>
+            Why do this?
+          </Typography>
+          <Typography variant="body1" textAlign="center">
+            We are developers who encourage each other to do better. It&apos;s
+            not just about writing code or programming hardware, it&apos;s about
+            making a positive impact in our community. We want to empower the
+            next generation of learners that will change our world and
+            hackathons are a great opportunity to do just that!
+          </Typography>
+        </Box>
+      </Grid>
     </Grid>
   );
 };
