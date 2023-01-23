f713a9192303bc72323845abdb72fa9e3a926f7d
Samuel Adetunji <guysnacho2@gmail.com>
2023-01-22 23:12:44 -0600

    Finish sponsorship package

--- lib/Firebase.ts
+++ lib/Firebase.ts
@@ -1,21 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { FirebaseOptions, initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const fbase = initializeApp(
-  firebaseConfig,
-  process.env.NEXT_PUBLIC_FIREBASE_APP_NAME
-);
+const fbase = initializeApp(firebaseConfig);
 
 export { fbase };
